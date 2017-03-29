cb35ccd58b189a670f485cf476b33a173d5cd155
Kristen Bateson <kbateson@uss-callcenter2.psu.ds.pdx.edu>
2017-03-29 15:50:23 -0700

    Initial commit

---
+++ Game.java
@@ -0,0 +1,31 @@
+package com.beautifulunicorntech;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ * Created by kbateson on 3/29/17.
+ */
+public class Game extends JFrame {
+    public Game() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new GameWindow());
+        setSize(250, 200);
+        setTitle("Jesus Slays");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Game ex = new Game();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
